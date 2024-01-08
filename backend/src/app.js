9467b281708d28cf46900f68471d987b145eb24b
Nhan Tran <rim25102004@gmail.com>
2024-01-08 23:33:24 +0200

    Fully Implementation backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -9,6 +10,19 @@ const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
+
+const apiURL = "mongodb+srv://tranhuynhdainhan:Baochau2903-@cluster0.ag2cn7s.mongodb.net/todoApp"
+// Connect to MongoDB
+mongoose.connect(apiURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
 app.use(morgan('dev'));
 app.use(helmet());
